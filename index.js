e1c2eaacb8a3109bdaccd1082fdc4b54e1c89b94
jscorza <jscorza@itba.edu.ar>
2023-02-19 16:25:51 -0300

    primer

--- index.js
+++ index.js
@@ -76,4 +76,3 @@ pmanager.addProduct( new Product("producto prueba ","Este es un producto prueba"
 console.log(pmanager.getProducts())
 pmanager.addProduct( new Product("producto prueba","Este es un producto prueba",200,"sin","abc123",25))
 console.log(pmanager.getProductById(1))
-console.log()
