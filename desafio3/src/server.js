21ab597f9269d273dbf6030fc55294432f0081cb
jscorza <jscorza@itba.edu.ar>
2023-03-08 16:11:20 -0300

    agregue productos y error en caso de no encontrar id

--- desafio3/src/server.js
+++ desafio3/src/server.js
@@ -1,11 +1,14 @@
 import express from "express"
 import ProductsManager from "./productManager.js"
+import http from "http"
 
 
-const manager = new ProductsManager()
+const path = "./files/productos.json"
+
+const manager = new ProductsManager(path)
 const app = express()
 
-app.get('/productos',async( pet , res) => {
+app.get('/products',async( pet , res) => {
     const products = await manager.getProducts()
     
     if(pet.query["limit"]){
@@ -19,10 +22,14 @@ app.get('/productos',async( pet , res) => {
     
 
 })
-app.get('/productos/:pid',async( pet , res) => {
+app.get('/products/:pid', async (pet, res) => {
     const id = pet.params.pid
     const producto = await manager.getProductById(id)
-    res.json(producto)
+    if (producto === undefined) {
+        res.json({ error: "El producto no fue encontrado" })
+    } else {
+        res.json(producto)
+    }
 })
 
 const server = app.listen(8080)
